4c0ef3a0a3d4d8e4dd60313a204bd7bc044311c2
qiandrewj <60044045+qiandrewj@users.noreply.github.com>
2020-12-25 15:12:06 -0500

    added database models into the app.locals

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var users = app.locals.users;
+var requests = app.locals.requests;
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
