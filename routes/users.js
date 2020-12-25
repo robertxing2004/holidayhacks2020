bda77c7c07d171eaf1f289a7b18492d55ab02ad1
scott-22 <scotthao65@gmail.com>
2020-12-25 13:52:59 -0500

    login stuff with github api

--- routes/users.js
+++ routes/users.js
@@ -10,27 +10,30 @@ router.get('/', function(req, res, next) {
 
 // Users login page
 router.get('/login', function(req, res, next) {
-  res.redirect(`https://github.com/login/oauth/authorize?client_id=${appinfo.clientID}`);
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${appinfo.clientID}&scope=user`);
 
 });
 
 // confirm login page (github callback)
 router.get('/confirmlogin', function(req, res, next) {
-  var client = req.app.locals.client;
+  //var client = req.app.locals.client;
 
   const body = {
     client_id: properties.clientID,
     client_secret: properties.clientSecret,
     code: req.query.code
   };
-
   const opts = { headers: { accept: 'application/json' } };
 
-  axios.post(`https://github.com/login/oauth/access_token`, body, opts).
+  axios.post('https://github.com/login/oauth/access_token', body, opts).
     then(res => {
       var token = res.data.access_token;
 
-      
+      console.log(`Token: ${token}\n`);
+      var data = axios.get('https://api.github.com/user', {headers: {authorization: `token ${token}`}}).data;
+
+      console.log(`\n\n\nData:${data}`);
+      //client.execute()//insert user info into database
     }).
     catch(err => res.status(500).json({ message: err.message }));
 })
