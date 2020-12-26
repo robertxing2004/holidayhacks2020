ab30e0f9d2ec78a8a86b82337ea7ba5352e870e1
scott-22 <scotthao65@gmail.com>
2020-12-26 15:29:58 -0500

    fixed up creating submissions

--- database.js
+++ database.js
@@ -18,25 +18,29 @@ var Schema = mongoose.Schema;
 var userSchema = new Schema({
    id:  String, //github login username
    email: String,
-   username: String,
-   points: Number,
+   username: String, //name of person
+   points: Number // points gained from giving gifts
 });
 
 var requestSchema = new Schema({
     id: String, //id of user who made the request
     description: String,
-    maxCost: String
+    maxCost: String // maximum cost allowed for the gift
 });
 
 var submitSchema = new Schema({ //pending submissions
+  _id: Schema.Types.ObjectId, // _id of corresponding request
   id: String, //id of user who made the submission
   requestid: String, //id of user who made the request
+  accepted: Boolean // whether a submission was accepted
 });
 
 var users = db.model('users', userSchema);
 var requests = db.model('requests', requestSchema);
+var submissions = db.model('submissions', submitSchema);
 
 module.exports = {
   users: users,
-  requests: requests
+  requests: requests,
+  submissions: submissions
 };
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios');
+var mongoose = require('mongoose');
 
 var properties = require('../properties');
 var database = require('../database');
@@ -37,7 +38,7 @@ router.get('/confirmlogin', function(req, res, next) {
     return (await axios.get('https://api.github.com/user', {headers: {authorization: `token ${token}`}})).data;
   }).
   then(async data => {
-    var query = await database.users.findOne({id: data.login});
+    let query = await database.users.findOne({id: data.login});
     if (!query) {
       let user = new database.users({
         id: data.login,
@@ -82,12 +83,10 @@ router.post('/submit', async function(req, res, next) {
       }
     }
     catch(err) {
-      console.log('\n\n\n******** error occured *********');
       console.log(err);
     }
   }
 
-  console.log('\n\n\n********\nRequests');
   console.log(requests);
   res.render('submit', {requests: requests});
 });
@@ -119,13 +118,26 @@ router.get('/createsubmit', function(req, res, next) {
 });
 
 // confirm intent to submit a gift for somebody else
-router.post('/addsubmission', function(req, res, next) {
-  if (!req.session.userid) res.send("Not logged in");
+router.post('/addsubmission', async function(req, res, next) {
+  if (!req.session.userid || !req.body.mongoId) res.send({message: "Error"});
   else {
-    console.log("confirming intent to submit gift");
+    console.log("\n\n\n*******\ncreating gift submission");
     console.log(req.body.mongoId);
-
-    res.send("Submission added!");
+    let submitquery = await database.submissions.findById(mongoose.Types.ObjectId(req.body.mongoId));
+    if (!submitquery) {
+      let requestquery = await database.requests.findById(mongoose.Types.ObjectId(req.body.mongoId));
+      let submission = new database.submissions({
+        _id: requestquery._id,
+        id: req.session.userid,
+        requestid: requestquery.id,
+        accepted: false
+      });
+      submission.save(err => {console.log(err);});
+      res.send({message: "Submission added!"});
+    }
+    else {
+      res.send({message: "Submission already exists!"});
+    }
   }
 });
 
